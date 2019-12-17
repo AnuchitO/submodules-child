89c789af0808ee6d7254ee68f96ea28a73deb548
AnuchitO <anuchitp@thoughtworks.com>
2019-12-17 12:21:59 +0700

    [submodule] friend update

--- child.js
+++ child.js
@@ -1,7 +1,8 @@
 
 
-const doSomething = () => {
+const doSomething = (friend, ownByme) => {
   console.log("do something ver.1")
+  console.log(`I'm your friend: ${friend}:::${ownByme}`)
 };
 
 exports.doSomething = doSomething;
